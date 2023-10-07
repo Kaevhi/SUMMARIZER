e0231f3b0041edcda8eb8dc9e8c6e5b6d74432f7
Kaevhi <ktotran27@gmail.com>
2023-10-07 10:01:59 -0400

    k
    k

--- JAVASCRIPT/outputPage.js
+++ JAVASCRIPT/outputPage.js
@@ -1,26 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let params = new URLSearchParams(window.location.search);
-    let videoId = params.get('video_id');
-    
-    if (videoId) {
-        fetch(`/transcript?video_id=${videoId}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                console.error(data.error);
-            } else {
-                let messagesContainer = document.querySelector('.messages');
-                messagesContainer.innerHTML = ""; // Clear out existing messages
-                
-                data.forEach(item => {
-                    let messageElement = document.createElement('p');
-                    messageElement.textContent = item.text;
-                    messagesContainer.appendChild(messageElement);
-                });
-            }
-        })
-        .catch(error => {
-            console.error("Error fetching transcript:", error);
-        });
+    const transcriptData = JSON.parse(localStorage.getItem('transcript'));
+
+    if (transcriptData) {
+        // Display the data on your page. For demonstration:
+        const messagesContainer = document.querySelector('.messages .response p');
+        messagesContainer.textContent = transcriptData[0]?.text || 'No transcript available';
     }
 });
